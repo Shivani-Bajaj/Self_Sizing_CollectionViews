215fc3448acd77de8e9f3d741c236e2bc4c268a6
Shivani-Bajaj <shivani.bajaj9@gmail.com>
2019-11-20 23:05:41 +0530

    Initial Commit

---
+++ Self_Sizing_CollectionViewTests/Self_Sizing_CollectionViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Self_Sizing_CollectionViewTests.swift
+//  Self_Sizing_CollectionViewTests
+//
+//  Created by Bajaj on 20/11/19.
+//  Copyright © 2019 Shivani Bajaj. All rights reserved.
+//
+
+import XCTest
+@testable import Self_Sizing_CollectionView
+
+class Self_Sizing_CollectionViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
